2069680f39f637b92cdd85b79ee748fc115b37da
thappeta <36814550+thappeta@users.noreply.github.com>
2021-04-15 18:38:00 +0530

    Add files via upload

---
+++ Double_Node.py
@@ -0,0 +1,5 @@
+class DNode:
+    def __init__(self,data):
+        self.data=data
+        self.prev=None
+        self.next=None
---
+++ circularlinkedlist.py
@@ -0,0 +1,49 @@
+from Node import Node
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, data):
+        new_node = Node(data)
+        cur = self.head
+        new_node.next = self.head
+
+        if self.head is None:
+            self.head=new_node
+            self.head.next=new_node
+        else:
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+        self.head = new_node
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else:
+            new_node = Node(data)
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+            new_node.next = self.head
+
+    def print_list(self):
+        cur = self.head
+
+        while cur:
+            print(cur.data)
+            cur = cur.next
+            if cur == self.head:
+                break
+
+
+if __name__ == '__main__':
+
+    cll_obj1 = CircularLinkedList()
+    for i in range(5):
+        cll_obj1.prepend(i)
+    cll_obj1.print_list()
